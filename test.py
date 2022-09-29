7eb64d405dd4daa5c289e926ebb9d6f539147f70
Su_daa <subin.kk019@gmail.com>
2022-09-29 17:58:30 +0900

    test

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print('test')
+import tkinter
+from tkinter import *
+
+#버튼 눌렀을때 처리
+def click_stbutton():
+
+
+
+#게임창 띄우기
+window=Tk()
+window.title("참참참 게임")
+window.geometry("700x700")
+
+#버튼 생성하기
+game_st=tkinter.Button(window,bg='white',text="게임 시작",command=click_stbutton())
+game_st.place(x=320,y=450)
+
+window.mainloop()
