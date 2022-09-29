09535c6f9696794c1d2f26f1b02aecb94b878f48
Su_daa <subin.kk019@gmail.com>
2022-09-30 00:11:37 +0900

    test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import tkinter
 from tkinter import *
 
 #버튼 눌렀을때 처리
-def click_stbutton():
+#def click_stbutton():
 
 
 
@@ -12,7 +12,7 @@ window.title("참참참 게임")
 window.geometry("700x700")
 
 #버튼 생성하기
-game_st=tkinter.Button(window,bg='white',text="게임 시작",command=click_stbutton())
+game_st=tkinter.Button(window,bg='white',text="게임 시작")
 game_st.place(x=320,y=450)
 
 window.mainloop()
