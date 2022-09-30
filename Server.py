97d75ee6080ae36b56228220d11e23c320f6b857
Uh-hub <tabacal3590@gmail.com>
2022-10-01 01:32:44 +0900

    하나의 컴퓨터에서 서버와 클라이언트 통신 network(client)-class 생성(서버 연결, 연결 후 data 전송) +서버와 network(client) 연결 여부 확인(연결O)

--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ import socket
 from _thread import *
 import sys
 
-server = "" #local host 이건 git 할 때 지우고 올리기. cmd->ipconfig 에서 하면 됨
+server = "" #IPv4주소: local host 이건 git 할 때 지우고 올리기. cmd->ipconfig 에서 하면 됨
 PORT = 60000
 
 #서버 소켓 생성
@@ -21,24 +21,28 @@ print('서버가 시작되었습니다. waiting for a connection')
 
 
 def threaded_client(conn):
+    conn.send(str.encode("Connected")) #연결된 상태
+    reply = "192.168.0.105"
+    #curi = "net?" #보내는 reply가 network.py에서 어떻게 뜨는지 확인하고 싶었음.
     while True: #이 함수가 클라이언트가 연결 되어있을 때도 계속 무한히 데이터를 받으면서 작동 해야 하기에
         try:
-            data = conn.recv(2048) #받으려는 데이터 양 2048 bit 수. 에러 뜨면 이거 사이즈 키우기
+            data = conn.recv(2048) #받으려는 데이터 양 2048 bit 수. 에러 뜨면 이거 사이즈 키우기/데이터를 받기 전까지 보내지 X
             reply = data.decode("utf-8") #받은 data는 우리가 읽을 수 있게 utf-8로 변경
 
             if not data: #data를 받지 않으면 연결 해제
                 print("Disconnected")
                 break
-            else: #에러 발생했을 경우를 대비해서
+            else: #data 받은 경우,
                 print("Received: ", reply)
                 print("Sending : ", reply)
 
             conn.sendall(str.encode(reply)) #정보를 보낼 떄 bit로 변경해서 전송
         except:
             print("threaded_client error")
-            break #그냥.. 딱히 에러 발생할 것 같지 않지만 일단 했기에.
+            break
 
-
+    print("Lost connection")
+    conn.close()
 
 #conn, addr = s.accept()
 
@@ -46,6 +50,6 @@ def threaded_client(conn):
 #connection과 그것의 IP 주소(addr)
 while True:
     conn, addr = s.accept()
-    print("Connected to:", addr)
+    print("Connected to:", addr) #client로 부터 주소를 받고 연결된 상태.
 
     start_new_thread(threaded_client, (conn,))
