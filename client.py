9a9ec1131d5634c261a8ae113a2623cf7608ea54
Su_daa <subin.kk019@gmail.com>
2022-10-01 23:12:16 +0900

    캐릭터 사진 선택 하기

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import tkinter
 import tkinter.messagebox
 import tkinter.simpledialog
 from tkinter import *
+from PIL import ImageTk,Image
 
 #새로운 프레임 띄우기
 def openFrame(frame):
@@ -36,6 +37,7 @@ def chat_send(e):
 window=tkinter.Tk()
 window.title("참참참 게임")
 window.geometry("700x700")
+window.resizable(False, False)
 
 frame1=tkinter.Frame(window,bg="red") #기본 프레임
 frame2=tkinter.Frame(window,bg="yellow") #두번째 프레임(닉네임 정하고 매칭 시작후 방 들어가는 화면)
@@ -52,23 +54,46 @@ frame3.grid(row=0, column=0, sticky="nsew")
 l_frame3.pack(side=tkinter.LEFT,fill="both",expand=True)
 r_frame3.pack(side=tkinter.RIGHT,fill="both",expand=True)
 
-#frame1
+#frame1(게임 첫 화면)
 game_st=tkinter.Button(frame1,bg='white',text="게임 시작",command = name_select)
 game_st.pack(padx=300,pady=500)
 
-#frame2
+#frame2(매칭방)
 input_nickname_bt=tkinter.Button(frame2,bg="white",text="방 입장",command=click_entrancebutton)
 input_nickname_bt.place(x = 315, y= 350)
 
-#frame3
-
+#frame3(대기방)
+frame3_cha=LabelFrame(l_frame3,text="캐릭터 선택창")
+frame3_cha.pack(fill="both",expand=True)
+frame3_chat=LabelFrame(r_frame3,text="채팅창")
+frame3_chat.pack(fill="both",expand=True)
 #캐릭터 선택 버튼
 char_select_br = tkinter.Button(l_frame3, text = "캐릭터 선택", command = character_select)
 char_select_br.place(x = 125,y= 260)
 
 #캐릭터 이미지
-char_image = tkinter.Canvas(l_frame3, width = 150, height = 150)
-char_image.place(x = 90, y = 100)
+image1=Image.open("image/image1.png")
+image1=image1.resize((200,200))
+image1=ImageTk.PhotoImage(image1)
+image2=Image.open("image/image2.jpg")
+image2=image2.resize((200,200))
+image2=ImageTk.PhotoImage(image2)
+image3=Image.open("image/image3.jpg")
+image3=image3.resize((200,200))
+image3=ImageTk.PhotoImage(image3)
+image4=Image.open("image/image4.jpg")
+image4=image4.resize((200,200))
+image4=ImageTk.PhotoImage(image4)
+
+
+char_image1 = tkinter.Button(frame3_cha,image=image1, width = 150, height = 150)
+char_image2 = tkinter.Button(frame3_cha,image=image2, width = 150, height = 150)
+char_image3 = tkinter.Button(frame3_cha,image=image3, width = 150, height = 150)
+char_image4 = tkinter.Button(frame3_cha,image=image4, width = 150, height = 150)
+char_image1.pack()
+char_image2.pack()
+char_image3.pack()
+char_image4.pack()
 
 #채팅 보여질 공간 (Label에서 Text로 변경)
 chat_space = tkinter.Text(r_frame3, width = 35, height = 30)
